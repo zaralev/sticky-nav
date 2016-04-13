0b3aa14463b49f0734f04335ec9d1eb07137ca93
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-04-13 10:22:21 -0400

    OMG I GOT THE HAMBURGER MENU TO WORK

---
+++ nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".mobile-dropdown").hide();
+  // $("#hamburger").click(function(){
+  //     $(".mobile-dropdown").show();
+  // });
+  $("#hamburger").click(function(){
+      $(".mobile-dropdown").toggle();
+  });
+});
